ce229c779c052f1a7d67589625376ac996e88e43
Villy Yeoh <yeohvilly@gmail.com>
2023-11-28 20:24:53 -0500

    filling up the seeds folder with some database

---
+++ seeds/seed.js
@@ -0,0 +1,44 @@
+const sequelize = require('../config/connection');
+const {Book, Review, User} = require('../models');
+
+const userData = require('./userData.json');
+const bookData = require('./bookData.json');
+const reviewData = require('./reviewData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    // Villy: I will come back and change it when necessary
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Book.bulkCreate(bookData);
+
+    await Review.bulkCreate(reviewData);
+}
+
+// Users:
+// ID
+// Username
+// Password
+// Email
+
+// Reviews:
+// ID
+// Title
+// User-id
+// Book-Id
+// Content
+
+// Books:
+// ID
+// Title
+// Author
+// Publisher
+// Genre
+// Description
+// Review
+// Page_Num
+
