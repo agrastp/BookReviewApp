df1ba59b35555a668e60b04b207abf677cc9df39
AgrastP <agrastp@yahoo.com>
2023-11-28 21:26:48 -0500

    edited routes/package.json

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./html-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
