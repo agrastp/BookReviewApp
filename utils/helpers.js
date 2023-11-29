6e03f0fc2dc07906501260a19f3603d4506fa9d8
DOOM Guy 1485 <theboss1485@gmail.com>
2023-11-28 20:58:29 -0500

    Added Initial Handlebars Views

---
+++ utils/helpers.js
@@ -0,0 +1,22 @@
+const {v4: uuidv4} = require('uuid');
+
+module.exports = {
+
+    // I, Gabriel, took these two helper functions from my module 14 challenge.
+    count: (index) => {
+
+        return index + 1;
+    },
+
+    equals: (argumentOne, argumentTwo) => {
+
+        if(argumentOne === argumentTwo){
+
+            return true;
+        
+        } else {
+
+            return false;
+        }
+    },
+}
